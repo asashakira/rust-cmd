ce058523b4c52be0067ae0734198c90829da07ab
akira <asa.shakira340@gmail.com>
2024-05-07 21:47:45 -0700

    add echo command

---
+++ echor/src/main.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(about, author, version)]
+struct Args {
+    #[arg(value_name = "TEXT", help = "Input text")]
+    text: Vec<String>,
+
+    #[arg(short = 'n', long, help = "do not output the trailing newline")]
+    omit_newline: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    print!("{}{}", args.text.join(" "), if args.omit_newline { "" } else { "\n" });
+}
